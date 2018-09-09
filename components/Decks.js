2cf1f7caacb9cd37de534db4d5335435ba0cb1fb
Juan Gomez <juanbertgomez@gmail.com>
2018-09-09 16:47:06 +0200

    initial commit

---
+++ components/Decks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Decks extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text> Hello Deck</Text>
+            </View>
+        )
+    }
+}
